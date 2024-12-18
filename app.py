63dd7a8ef36f483b8e496dd09afb90ba1a21892e
BryanKnnd <bryan.kenned@academico.ifpb.edu.br>
2024-12-18 13:49:11 -0300

    Primeiro commit.

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+print(__file__)
+app = Flask(__name__)
+
+# ROTA PRINCIPAL
+@app.route("/", methods=["GET"])
+@app.route("/cadastro-compras", methods=["GET"])
+def cadastro_compras():
+    return render_template("cadastro_compras.html")
+
+# ROTA PARA RECEBER OS DADOS DO FORMULÁRIO DE cadastro_compras.html
+# E COLOCA-LOS NO ARQUIVO JSON
+@app.route("/adicionar-produto-no-json", methods=["POST"])
+def adicionar_produto_no_json():
+    # caminho do arquivo: /home/aluno/Documentos/novo-gerenciafacil/produtos_cadastrados.json
+    ...
+
+if __name__ == "__main__":
+    app.run()
