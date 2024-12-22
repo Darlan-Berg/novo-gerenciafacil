b63948ff68bf560ebf62e4ca00d7cccaf853c5a5
Darlan-Berg <darlanbergsilva@gmail.com>
2024-12-22 16:40:43 -0300

    Início da implementação da funcionalidade de cadastro de compras

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template, request
-print(__file__)
+from flask import Flask, render_template, request, redirect, url_for
+import json
+import os
+
 app = Flask(__name__)
 
 # ROTA PRINCIPAL
@@ -12,8 +14,31 @@ def cadastro_compras():
 # E COLOCA-LOS NO ARQUIVO JSON
 @app.route("/adicionar-produto-no-json", methods=["POST"])
 def adicionar_produto_no_json():
-    # caminho do arquivo: /home/aluno/Documentos/novo-gerenciafacil/produtos_cadastrados.json
-    pass
+    if request.method == "POST":
+        # coletar dados do form
+        nome = request.form.get("nome")
+        qtd = request.form.get("qtd")
+        data_validade = request.form.get("dataValidade")
+
+        # criar um novo produto
+        novo_produto = {
+            "nome": nome,
+            "qtd": qtd,
+            "dataValidade": data_validade
+        }
+
+        # escrever novos dados em produtos_cadastrados.json
+        caminho_dir_trab = os.getcwd()
+        caminho_arq_json = os.path.join(caminho_dir_trab, "produtos_cadastrados.json")
+        
+        with open(caminho_arq_json, "r") as arq_json:
+            conteudo = json.load(arq_json)
+            lista_produtos: list = conteudo["listaProdutos"]
+            lista_produtos.append(novo_produto)
+        with open(caminho_arq_json, "w") as arq_json:
+            json.dump(conteudo, arq_json, indent=4)
+
+        return redirect(url_for("cadastro_compras"))
 
 if __name__ == "__main__":
     app.run(debug=True)
